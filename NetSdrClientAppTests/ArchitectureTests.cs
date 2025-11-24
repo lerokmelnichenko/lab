1a82e7ec8e668234a93a6f3dae55dbd766a17f2d
lerokmelnichenko <lerokmelnichenko@gmail.com>
2025-11-24 16:38:00 +0200

    Add architecture tests to enforce dependency rules between messages & networking and server & client (Lab 5)

---
+++ NetSdrClientAppTests/ArchitectureTests.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Reflection;
+using NetArchTest.Rules;
+using NetSdrClientApp;                  // тип із клієнтського застосунку
+using NUnit.Framework;
+
+namespace NetSdrClientAppTests
+{
+    [TestFixture]
+    public class ArchitectureTests
+    {
+
+        [Test]
+        public void Messages_ShouldNot_Depend_On_Networking()
+        {
+            var result = Types
+                .InAssembly(Assembly.Load("NetSdrClientApp"))
+                .That()
+                .ResideInNamespace("NetSdrClientApp.Messages")
+                .ShouldNot()
+                .HaveDependencyOn("NetSdrClientApp.Networking")
+                .GetResult();
+
+            Assert.That(
+                result.IsSuccessful,
+                Is.True,
+                "Messages має заборонені залежності від Networking"
+            );
+        }
+
+        [Test]
+        public void Client_ShouldNot_Depend_On_Server()
+        {
+            var result = Types
+                .InAssembly(Assembly.Load("NetSdrClientApp"))
+                .ShouldNot()
+                .HaveDependencyOn("EchoServer")
+                .GetResult();
+
+            Assert.That(
+                result.IsSuccessful,
+                Is.True, 
+                "Продакшен-код не повинен залежати від тестового проєкту, але є залежності"
+            );
+        }
+    }
+}
