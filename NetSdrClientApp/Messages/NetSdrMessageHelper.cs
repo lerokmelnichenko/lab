0c8fc5d6a60159bd4e270c6e044a0b2fd58133c3
lerokmelnichenko <lerokmelnichenko@gmail.com>
2025-11-25 22:51:05 +0200

    Use a constructor overloads that allows a more meaningful exception message to be provided.

--- NetSdrClientApp/Messages/NetSdrMessageHelper.cs
+++ NetSdrClientApp/Messages/NetSdrMessageHelper.cs
@@ -103,7 +103,7 @@
             sampleSize /= 8; //to bytes
             if (sampleSize  > 4)
             {
-                throw new ArgumentOutOfRangeException();
+                throw new ArgumentOutOfRangeException(nameof(sampleSize), "Sample size in bytes cannot exceed 4");
             }
 
             var bodyEnumerable = body as IEnumerable<byte>;
